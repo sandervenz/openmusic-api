ff9a1de19284c15bd98d22a9404c8f5b58cb8583
Sander <sandervdb23@gmail.com>
2025-03-13 15:03:09 +0700

    database migration and server init

---
+++ migrations/1741852716766_create-table-songs.js
@@ -0,0 +1,16 @@
+exports.up = (pgm) => {
+    pgm.createTable('songs', {
+      id: { type: 'VARCHAR(50)', primaryKey: true },
+      title: { type: 'TEXT', notNull: true },
+      year: { type: 'INTEGER', notNull: true },
+      genre: { type: 'TEXT', notNull: true },
+      performer: { type: 'TEXT', notNull: true },
+      duration: { type: 'INTEGER' },
+      album_id: { type: 'VARCHAR(50)', references: '"albums"(id)', onDelete: 'CASCADE' }
+    });
+  };
+  
+  exports.down = (pgm) => {
+    pgm.dropTable('songs');
+  };
+  
