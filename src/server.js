8dc9e40e0219de227a210fb5cf75a1623f6ad4af
Sander <sandervdb23@gmail.com>
2025-03-13 15:26:28 +0700

    add validation and exception handling

--- src/server.js
+++ src/server.js
@@ -1,12 +1,24 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+const AlbumsService = require('./services/postgresql/AlbumsService');
+const SongsService = require('./services/postgresql/SongsService');
+const albums = require('./api/albums');
+const songs = require('./api/songs');
 
 const init = async () => {
+  const albumsService = new AlbumsService();
+  const songsService = new SongsService();
+
   const server = Hapi.server({
-    port: process.env.PORT || 5000,
-    host: process.env.HOST || 'localhost',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: { cors: { origin: ['*'] } },
   });
+
+  await server.register([
+    { plugin: albums, options: { service: albumsService } },
+    { plugin: songs, options: { service: songsService } },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
